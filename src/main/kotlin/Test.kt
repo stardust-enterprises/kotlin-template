6709a97eeefc06c6d78602a27e4b60d10ba93919
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-04-16 11:16:45 +0200

    main: demonstrate Kotlin 1.6.20 features

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,6 +1,13 @@
 fun main() {
+    val testImpl = ITest { "Hello, world!" }
+    with(testImpl) { printTest() }
+
     val projectName = "@project.name@"
     val projectVersion = "@project.version@"
     println("Project: $projectName v$projectVersion")
     println("Description: @project.desc@")
+}
+
+context(ITest) fun printTest() {
+    println(string())
 }
