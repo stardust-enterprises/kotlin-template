f4fa6437afab82ba053609321a4bbbe1e1db1ba7
killian <xtram.dev@gmail.com>
2022-09-11 11:09:05 +0200

    Update Dependencies.kt

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,12 +1,12 @@
-private const val kotlinVersion = "1.6.20"
+private const val kotlinVersion = "1.7.10"
 
 object Plugins {
     const val KOTLIN = kotlinVersion
     const val GRGIT = "4.1.1" // old version for jgit to work on Java 8
-    const val BLOSSOM = "1.3.0"
+    const val BLOSSOM = "1.3.1"
     const val SHADOW = "7.1.2"
-    const val KTLINT = "10.2.1"
-    const val DOKKA = "1.6.20"
+    const val KTLINT = "11.0.0"
+    const val DOKKA = "1.7.10"
     const val NEXUS_PUBLISH = "1.0.0"
 }
 
